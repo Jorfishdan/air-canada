266bca905d000c1698e108dd03606a777eb68725
Jorfishdan <jordan_fish@ymail.com>
2023-02-02 10:16:43 -0800

    images added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import magnify from "../src/assets/images/magnify.svg";
+import logoDark from "../src/assets/images/logoDark.svg";
+import logoLight from "../src/assets/images/logoLight.svg";
+import logoLong from "../src/assets/images/logoLong.svg";
+import arrows from "../src/assets/images/arrows.svg";
+import info from "../src/assets/images/info.svg";
+import star from "../src/assets/images/star.svg";
+import plane from "../src/assets/images/plane.svg";
+import calendar from "../src/assets/images/calendar.svg";
+import socials from "../src/assets/images/socials.svg";
+
 
 function App() {
   return (
     <div className="App">
       <p>hiii</p>
+      <img src={magnify} alt="this is a test" />
+      <img src={logoDark} alt="this is a test" />
+      <img src={logoLight} alt="this is a test" />
+      <img src={logoLong} alt="this is a test" />
+      <img src={arrows} alt="this is a test" />
+      <img src={info} alt="this is a test" />
+      <img src={star} alt="this is a test" />
+      <img src={plane} alt="this is a test" />
+      <img src={calendar} alt="this is a test" />
+      <img src={socials} alt="this is a test" />
+
     </div>
   );
 }
