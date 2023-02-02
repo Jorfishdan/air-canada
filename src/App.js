1f6d63fb91b30dfd8e13ab29a2d3660441c3717f
marcor93 <mrizzo93@gmail.com>
2023-02-02 15:48:09 -0800

    added footer on desktop via img

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ import star from "../src/assets/images/star.svg";
 import plane from "../src/assets/images/plane.svg";
 import calendar from "../src/assets/images/calendar.svg";
 import socials from "../src/assets/images/socials.svg";
-
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <p className="App__test1">normal font</p>
-      <p className="App__test2">bold font</p>
-      <p className="App__test3">semi-bold font</p>
-      <img src={magnify} alt="this is a test" />
-      <img src={logoDark} alt="this is a test" />
-      <img src={logoLight} alt="this is a test" />
-      <img src={logoLong} alt="this is a test" />
-      <img src={arrows} alt="this is a test" />
-      <img src={info} alt="this is a test" />
-      <img src={star} alt="this is a test" />
-      <img src={plane} alt="this is a test" />
-      <img src={calendar} alt="this is a test" />
-      <img src={socials} alt="this is a test" />
-
-    </div>
+    <>
+      <div className="App">
+        <p className="App__test1">normal font</p>
+        <p className="App__test2">bold font</p>
+        <p className="App__test3">semi-bold font</p>
+        <img src={magnify} alt="this is a test" />
+        <img src={logoDark} alt="this is a test" />
+        <img src={logoLight} alt="this is a test" />
+        <img src={logoLong} alt="this is a test" />
+        <img src={arrows} alt="this is a test" />
+        <img src={info} alt="this is a test" />
+        <img src={star} alt="this is a test" />
+        <img src={plane} alt="this is a test" />
+        <img src={calendar} alt="this is a test" />
+        <img src={socials} alt="this is a test" />
+      </div>
+      <Footer />
+    </>
   );
 }
 
