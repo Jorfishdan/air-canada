5affe24fd4932de9aadeec5ca05abdf558560ca8
marcor93 <mrizzo93@gmail.com>
2023-02-03 09:43:20 -0800

    added defualt rout and linking

--- src/components/MichaelHero/MichaelHero.js
+++ src/components/MichaelHero/MichaelHero.js
@@ -1,19 +1,25 @@
-import './MichaelHero.scss';
+import "./MichaelHero.scss";
 // import StarbucksHero from '../../assets/images/starbucks-hero.png';
-import creditCard from "../../assets/images/creditCard.svg"
+import creditCard from "../../assets/images/creditCard.svg";
 
 function MichaelHero() {
-    return (
-        <section className="michael">
-            <div className="michael__image-container">
-                <img className="michael__image" src={creditCard} alt="Starbucks Offer Image"></img>
-            </div>
-            <div className="michael__text-container">
-                <h1 className="michael__title">Good afternoon, Michael</h1>
-                <p className="michael__tagline">Earn faster and travel better with an Aeroplan credit card.</p>
-                <button className="michael__button">Learn More</button>
-            </div>
-        </section>
-    )
+  return (
+    <section className="michael">
+      <div className="michael__image-container">
+        <img
+          className="michael__image"
+          src={creditCard}
+          alt="Starbucks Offer Image"
+        ></img>
+      </div>
+      <div className="michael__text-container">
+        <h1 className="michael__title">Good afternoon, Michael</h1>
+        <p className="michael__tagline">
+          Earn faster and travel better with an Aeroplan credit card.
+        </p>
+        <button className="michael__button">Learn More</button>
+      </div>
+    </section>
+  );
 }
 export default MichaelHero;
