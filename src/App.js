e870979e174ce53db10794295a5d693b3bbb0a4f
Jorfishdan <jordan_fish@ymail.com>
2023-02-03 09:12:23 -0800

    added favs page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./pages/Homepage/Homepage";
+
 function App() {
   return (
     <>
---
+++ src/components/MichaelCardFavorite/MichaelCardFavorite.js
@@ -0,0 +1,39 @@
+import "./MichaelCardFavorite.scss";
+import ssense from  "../../assets/images/ssense.png";
+import lulu from "../../assets/images/lulu.png";
+import apple from "../../assets/images/apple.png";
+import bookMark from "../../assets/images/bookMark.png"
+
+
+function MichaelCardFavorite() {
+  return (
+    <section className="favorite">
+      <div className="favoriteheader">
+        <img className="card__bookmark" src={bookMark} alt="bookmark" />
+        <h1 className="favoriteheader__title">Favorite Stores</h1>
+      </div>
+      <div className="favoritewrapper">
+        <div className="cardfavorite">
+          <img className="card__ssense" src={ssense} alt="get ssense" />
+  
+        </div>
+        <div className="cardfavorite">
+        <img className="card__ssense" src={lulu} alt="get ssense" />
+         
+        </div>
+        <div className="cardfavorite">
+        <img className="card__ssense" src={apple} alt="get ssense" />
+      
+        </div>
+      </div>
+      <div className="favebuttons">
+        <p className="favebuttons__1">Browse Offers</p>
+        <p className="favebuttons__2">Browse Offers</p>
+        <p className="favebuttons__3">Browse Offers</p>
+      </div>
+      <p className="favoritebottom">View All > </p>
+    </section>
+  );
+}
+
+export default MichaelCardFavorite;
