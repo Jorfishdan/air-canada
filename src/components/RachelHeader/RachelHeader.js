2098e72f0678a48a8518042826c3f2392307d4b7
marcor93 <mrizzo93@gmail.com>
2023-02-03 09:30:15 -0800

    created unique routes for each profile

---
+++ src/components/RachelHeader/RachelHeader.js
@@ -0,0 +1,51 @@
+import "./RachelHeader.scss";
+import HeaderLogo from "../../assets/images/header-logo-dark.PNG";
+import GlobeImage from "../../assets/images/language_white_24dp.svg";
+import AccountImage from "../../assets/images/perm_identity_white_24dp.svg";
+import MenuIcon from "../../assets/images/menu_white_24dp.svg";
+import Navbar from "../Navbar/Navbar";
+
+export default function RachelHeader() {
+  return (
+    <>
+      <header className="rachelheader">
+        <div className="rachelheader__logo-container">
+          <img
+            className="rachelheader__menu-logo"
+            src={MenuIcon}
+            alt="menu icon"
+          ></img>
+          <img
+            className="rachelheader__logo"
+            src={HeaderLogo}
+            alt="air canada logo"
+          ></img>
+        </div>
+        <nav className="rachelheader-nav">
+          <section className="rachelheader-nav__links">
+            <button className="rachelheader-nav__search">Search</button>
+            <button className="rachelheader-nav__access">Accessibility</button>
+            <div className="rachelheader-nav__language-container">
+              <img
+                className="rachelheader-nav__globe"
+                alt="globe logo"
+                src={GlobeImage}
+              ></img>
+              <button className="rachelheader-nav__language">English</button>
+            </div>
+          </section>
+          <section className="rachelheader-nav__account">
+            <img
+              className="rachelheader-nav__account-logo"
+              alt="account logo"
+              src={AccountImage}
+            ></img>
+            <span className="rachelheader-nav__info">Rachel</span>
+            <span className="rachelheader-nav__info">10,000 pts</span>
+          </section>
+        </nav>
+      </header>
+      <Navbar />
+    </>
+  );
+}
