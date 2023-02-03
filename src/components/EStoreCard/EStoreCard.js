25515a65ca66fb11d54d664f04ab14d3bea7e83a
Jorfishdan <jordan_fish@ymail.com>
2023-02-02 21:51:52 -0800

    images added

--- src/components/EStoreCard/EStoreCard.js
+++ src/components/EStoreCard/EStoreCard.js
@@ -1,6 +1,9 @@
 import "./EStoreCard.scss";
 import viewEight from "../../assets/images/viewEight.png";
 import theRedBag from "../../assets/images/theRedBag.png";
+import dyson from "../../assets/images/dyson.png";
+import rename from "../../assets/images/rename.png";
+import bestBuy from "../../assets/images/bestBuy.png";
 
 function eStoreCard() {
   return (
@@ -13,9 +16,15 @@ function eStoreCard() {
       <img className="eStore__bag" src={theRedBag} alt="shopping bag icon"/>
       </div>
       <div className="wrapper__wrapper">
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      <div className="card">
+        <img className="card1" src={rename} alt="recieve reminders" />
+      </div>
+      <div className="card">
+        <img className="card2" src={bestBuy} alt="get Best Buy" />
+      </div>
+      <div className="card">
+        <img className="card3" src={dyson} alt="get a dyson" />
+      </div>
       </div>
     </section>
   );
