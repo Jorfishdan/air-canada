2ca6a230c60a52ed124f23ab52f6af4427597b52
Han Kim <hanwkim88@gmail.com>
2023-02-03 13:29:45 -0500

    Add wrapper for elite section.

--- src/pages/Michael/Michael.js
+++ src/pages/Michael/Michael.js
@@ -1,20 +1,12 @@
 import "./Michael.scss";
 import MichaelHero from "../../components/MichaelHero/MichaelHero";
-import MichaelBanner from "../../components/MichaelBanner/MichaelBanner";
-import UpcomingTrips from "../../components/UpcomingTrips/UpcomingTrips";
-import MichaelCardFavorite from "../../components/MichaelCardFavorite/MichaelCardFavorite";
-import EliteStatusPerks from "../../components/EliteStatusPerks/EliteStatusPerks";
-import KeepStatus from "../../components/KeepStatus/KeepStatus";
+import EliteWrapper from "../../components/EliteWrapper/EliteWrapper";
 
 function Michael () {
     return(
         <>
         <MichaelHero />
-        <MichaelBanner />
-        <UpcomingTrips />
-        <EliteStatusPerks />
-        <KeepStatus />
-        <MichaelCardFavorite />
+        <EliteWrapper />
         </>
     )
 
