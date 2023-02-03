71cefd4a4798d3060e0255275aa5ac814cf6085b
Han Kim <hanwkim88@gmail.com>
2023-02-02 23:12:09 -0500

    Add hero section with styling.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Card from "./components/Card/Card";
 import ExclusiveOffersBar from "./components/ExclusiveOffersBar/ExclusiveOffersBar";
+import Hero from "./components/Hero/Hero";
 
 
 function App() {
 	return (
 		<>
 			<Header />
+      <Hero />
       <ExclusiveOffersBar />
       <Card />
 			<Footer />
