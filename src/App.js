ff3e78bbd2a0fd7043f80c6e743287ca25e69103
marcor93 <mrizzo93@gmail.com>
2023-02-03 00:21:20 -0800

    fixed homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import ExclusiveOffersBar from "./components/ExclusiveOffersBar/ExclusiveOffersBar";
-import Hero from "./components/Hero/Hero";
-import CardUnlock from "./components/CardUnlock/CardUnlock";
-import CurrentDealsCard from "./components/CurrentDealsCard/CurrentDealsCard";
-import EStoreCard from "./components/EStoreCard/EStoreCard";
-import DoMore from "./components/DoMore/DoMore";
-
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <ExclusiveOffersBar />
-      <CardFavorite />
-      <CurrentDealsCard />
-      <EStoreCard />
-      <CardUnlock />
-      <DoMore />
-			<Footer />
-		</>
-	);
+      <Homepage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
