b809ffeec447e48a29d3a346d56a8bbb6bde45d0
marcor93 <mrizzo93@gmail.com>
2023-02-02 20:05:55 -0800

    adding favorite card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.scss";
-import Card from "./components/Card/Card";
+import CardFavorite from "./components/CardFavorite/CardFavorite";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-      <Card />
+      <CardFavorite />
       <Footer />
     </>
   );
