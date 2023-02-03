63a89a1677b1a5d4776c8e1efd1b7d01e94214bf
marcor93 <119454338+marcor93@users.noreply.github.com>
2023-02-02 18:39:57 -0800

    Revert "Create cardwrapper"

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import "./Footer.scss";
-import dtfooter from "../../assets/images/Footer-desktop.png";
-
-function Footer() {
-  return <img src={dtfooter} alt="footer banner" className="footer" />;
-}
-
-export default Footer;
