8964fdc771eaeea7f48d7b4832deca3339bdf14a
Jorfishdan <jordan_fish@ymail.com>
2023-02-02 19:10:48 -0800

    hovers and changes colors

--- src/components/ExclusiveOffersBar/ExclusiveOffersBar.js
+++ src/components/ExclusiveOffersBar/ExclusiveOffersBar.js
@@ -8,23 +8,23 @@ function ExclusiveOffersBar() {
     return (
         <>
         <div className="exOffers">
-        <p className="exOffers__header">Your exclusive offers</p>
+        {/* <p className="exOffers__header">Your exclusive offers</p> */}
         <article className="exOffers__main-wrapper">
             <div className="exOffers__wrapper">
-            <img className="exOffers__list"src={list} alt="list icon" />
-            <p className="exOffers__list-label">View all</p>
+            {/* <img className="exOffers__list"src={list} alt="list icon" /> */}
+            <p className="exOffers__everyday-essentials">Everyday Essential</p>
             </div>
             <div className="exOffers__wrapper">
-            <img className="exOffers__everyday" src={redeem} alt="credit card icon" />
-            <p className="exOffers__everyday-label">Everyday</p>
+            {/* <img className="exOffers__everyday" src={redeem} alt="credit card icon" /> */}
+            <p className="exOffers__travel">Travel</p>
             </div>
             <div className="exOffers__wrapper">
-            <img className="exOffers__plane" src={plane} alt="plane icon" />
-            <p className="exOffers__plane-label">Travel</p>
+            {/* <img className="exOffers__plane" src={plane} alt="plane icon" /> */}
+            <p className="exOffers__eStore">eStore</p>
             </div>
             <div className="exOffers__wrapper">
-            <img className="exOffers__bag" src={bag} alt="shopping bag icon" />
-            <p className="exOffers__bag-label">eStore</p>
+            {/* <img className="exOffers__bag" src={bag} alt="shopping bag icon" /> */}
+            <p className="exOffers__manage">Manage Your Page</p>
             </div>
             
         </article>
