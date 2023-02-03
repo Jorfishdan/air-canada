8ab0d421ec8ece2d27ea0d5adb1bf5e00732bf46
Han Kim <hanwkim88@gmail.com>
2023-02-03 01:50:36 -0500

    Merge develop.

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,18 +4,20 @@ import ExclusiveOffersBar from "../../components/ExclusiveOffersBar/ExclusiveOff
 import CardFavorite from "../../components/CardFavorite/CardFavorite";
 import CurrentDealsCard from "../../components/CurrentDealsCard/CurrentDealsCard";
 import Modal from "../../components/Modal/Modal";
+import DoMore from "./components/DoMore/DoMore";
+import EveryDay from "./components/EveryDay/EveryDay";
 import { useState } from "react";
 
 export default function Homepage() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	return (
 		<main className="main">
-            {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+			{isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
 			<Hero />
 			<ExclusiveOffersBar setIsModalOpen={setIsModalOpen} />
-			<CardFavorite />
-			<CurrentDealsCard />
+			<EveryDay />
+			<DoMore />
 		</main>
 	);
 }
