93f6c48d97eb92c40babaad7ae08f23fc2767e46
Jorfishdan <jordan_fish@ymail.com>
2023-02-02 19:29:17 -0800

    new exOffers Bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Card from "./components/Card/Card";
+import ExclusiveOffersBar from "./components/ExclusiveOffersBar/ExclusiveOffersBar";
 
 
 function App() {
 	return (
 		<>
 			<Header />
+      <ExclusiveOffersBar />
       <Card />
 			<Footer />
 		</>
